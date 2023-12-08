3fc04eb8a766a04942632350a1a254ca4e2e1c7b
Sai Merneedi <pmern2@example.com>
2023-12-08 14:32:46 -0600

    model done

--- model.py
+++ model.py
@@ -1,39 +1,53 @@
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import FunctionTransformer
 
-lecture_transcripts = [] # TODO
-assignment_descriptions = [] # TODO
+RELEVANCE_THRESHOLD = 0.3
+
+# Step 1: Preprocess the Data
+def preprocess_text(text):
+    # Implement text cleaning here (e.g., lowercasing, removing special characters)
+    return text.lower()
+
+# Load CSV
+df = pd.read_csv('./data/lecturedata.csv') # Update with the actual path
+#print(df)
+transcripts = df['Transcript'].apply(preprocess_text)
+
+# Step 2: Vectorizing the Text
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(transcripts)
+
+# Step 3 & 4: Search Function
+def find_relevant_lectures(query, top_n=3):
+    query_vec = vectorizer.transform([preprocess_text(query)])
+    cosine_similarities = cosine_similarity(query_vec, tfidf_matrix).flatten()
+
+    # Filter based on threshold
+    relevant_lectures = [(index, score) for index, score in enumerate(cosine_similarities) if score >= RELEVANCE_THRESHOLD]
+    
+    # Sort based on scores and get top_n indices
+    relevant_lectures.sort(key=lambda x: x[1], reverse=True)
+    relevant_lectures_indices = [index for index, score in relevant_lectures][:top_n]
+
+    return df['Lecture'].iloc[relevant_lectures_indices].to_list()
 
 
-# preprocess the data so its uniform
-def preprocess(text):
-    # lowercase and remove punctuation
-    text = text.lower()
-    text = ''.join([c for c in text if c.isalpha() or c.isspace()])
+# Example Usage
+query = '''
+In this MP, you will get familiar with building and evaluating Search Engines.
 
-    return text
+Part 1
+In this part, you will use the MeTA toolkit to do the following:
 
-lecture_transcripts = [preprocess(lecture) for lecture in lecture_transcripts]
-assignment_descriptions = [preprocess(assignment) for assignment in assignment_descriptions]
+create a search engine over a dataset
+investigate the effect of parameter values for a standard retrieval function
+write the InL2 retrieval function
+investigate the effect of the parameter value for InL2
+Choose one of the above retrieval functions and one of its parameters (don’t choose BM25 + k3, it’s not interesting). For example, you could choose Dirichlet Prior and mu.
 
+Change the ranker to your method and parameters. In the example, it is set to bm25. Use at least 10 different values for the parameter you chose; try to choose the values such that you can find a maximum MAP.
+'''
 
-vectorizer = make_pipeline(
-    TfidfVectorizer(stop_words=ENGLISH_STOP_WORDS),
-    FunctionTransformer(lambda x: x.astype('float'), validate=False)
-)
-
-tfidf_matrix = vectorizer.fit_transform(lecture_transcripts + assignment_descriptions)
-
-# Calculate cosine similarity, could use other similarity function as well
-cosine_similarities = cosine_similarity(tfidf_matrix[-len(assignment_descriptions):], tfidf_matrix[:-len(assignment_descriptions)])
-
-# Match assignments to lectures based on cosine similarity
-for i, assignment in enumerate(assignment_descriptions):
-    relevant_lectures = cosine_similarities[i].argsort()[:-4:-1]  # Get top 3 most similar lectures
-    print(f"\nAssignment: {assignment}")
-    print("Relevant Lecture Videos:")
-    for j in relevant_lectures:
-        print(f"- Lecture {j + 1} (Similarity Score: {cosine_similarities[i][j]:.4f})")
+relevant_lectures = find_relevant_lectures(query)
+print("Lecture Numbers: ", relevant_lectures)
